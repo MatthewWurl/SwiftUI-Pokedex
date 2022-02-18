148afa602ed72d8f83f119504d87453219efce56
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-02-17 20:21:00 -0500

    Add base SwiftUI project code

---
+++ SwiftUI-Pokedex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Pokedex
+//
+//  Created by Matt X on 2/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Pokedex/SwiftUI_PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PokedexApp.swift
+//  SwiftUI-Pokedex
+//
+//  Created by Matt X on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
