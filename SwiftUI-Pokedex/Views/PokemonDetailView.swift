60900242012dbf1fcf11f111bf85b32a902b042c
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-02-22 00:53:51 -0500

    Create PokemonDetailView.swift

---
+++ SwiftUI-Pokedex/Views/PokemonDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonDetailView.swift
+//  SwiftUI-Pokedex
+//
+//  Created by Matt X on 2/22/22.
+//
+
+import SwiftUI
+
+struct PokemonDetailView: View {
+    var body: some View {
+        Text("Pokemon Detail View")
+    }
+}
+
+struct PokemonDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonDetailView()
+    }
+}
