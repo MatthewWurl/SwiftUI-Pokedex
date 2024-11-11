a45ef96efcde49bfffd38301e7380322c58887e7
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2024-11-11 00:47:03 -0500

    Update pokemon cell to use background modifier instead of ZStack

--- SwiftUI-Pokedex/Views/PokemonCell.swift
+++ SwiftUI-Pokedex/Views/PokemonCell.swift
@@ -12,34 +12,33 @@ struct PokemonCell: View {
     let result: PokemonResult
     
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .foregroundColor(Color(result.colorName))
-            
-            HStack {
-                VStack(alignment: .leading, spacing: 10) {
-                    Text(result.name.capitalized)
-                        .font(.subheadline.bold())
-                        .foregroundColor(.white)
-                        .lineLimit(1)
-                        .minimumScaleFactor(0.6)
-                    
-                    HStack(spacing: 5) {
-                        ForEach(result.typeIconNames, id: \.self) { typeIcon in
-                            Image(typeIcon)
-                                .typeIconStyle()
-                        }
+        HStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(result.name.capitalized)
+                    .font(.subheadline.bold())
+                    .foregroundColor(.white)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.6)
+                
+                HStack(spacing: 5) {
+                    ForEach(result.typeIconNames, id: \.self) { typeIcon in
+                        Image(typeIcon)
+                            .typeIconStyle()
                     }
                 }
-                
-                Spacer()
-                
-                KFImage(result.imageUrl)
-                    .resizable()
-                    .frame(width: 50, height: 50)
             }
-            .padding(.vertical, 20)
-            .padding(.horizontal, 10)
+            
+            Spacer()
+            
+            KFImage(result.imageUrl)
+                .resizable()
+                .frame(width: 50, height: 50)
+        }
+        .padding(.vertical, 20)
+        .padding(.horizontal, 10)
+        .background {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(Color(result.colorName))
         }
     }
 }
